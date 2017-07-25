44718f31545ec0a611c59bbdc04dc5699157cd3a
rafaelbasile <rafaelbasile@gmail.com>
2017-07-24 21:14:00 -0300

    settings

--- website/settings.py
+++ website/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(czm*g(8&$qiyr3gj)n*slbk+6qm&lq#pf-9=!6@+^t&vmrz27'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','performancesap.com','www.performancesap.com']
 
 
 # Application definition
